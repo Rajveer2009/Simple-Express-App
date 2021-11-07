71e69401b87cb043a542b6a92d6fbbee3e7e8697
Rajveer <93126793+Rajveer2009@users.noreply.github.com>
2021-11-07 20:25:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-//importing Libraries
 const express = require('express');
 const app = express();
 
-//listening for Requests and giving back Response
 app.get('/' , (req, res) => {
-    console.log('request was made: ' + req.url);
-    res.send({ data: 'Hello'});
-});
-app.get('/name' , (req, res) => {
-    console.log('request was made: ' + req.url);
-    res.send({ data: 'Rajveer'});
-});
-app.get('/age' , (req, res) => {
-    console.log('request was made: ' + req.url);
-    res.send({ data: 22});
+    console.log(req.url)
+    res.send("Hello, World!")
 });
 
-//serving it to the localhost port:8080
 app.listen(8080);
